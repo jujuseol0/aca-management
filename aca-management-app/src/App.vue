142ea9036b69353895bcbc4ee361e4bd720d8bd7
jujuseol0 <jujuseol0@gmail.com>
2021-09-17 16:18:22 +0900

    create: create vue 2.x project

---
+++ aca-management-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon/>
+      <v-toolbar-title>Page Title</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
